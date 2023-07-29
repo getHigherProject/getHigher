ab77440ca9c03ee950bd0d526e4d97fb3ada65c8
Darren Pavel <darren.pavel@gmail.com>
2023-07-29 12:13:49 -0700

    tests created, and databases defined.

---
+++ server/envVars.js
@@ -0,0 +1,3 @@
+const PG_URI = 'postgres://hjvoycry:qkhROnLga40l3CNoTB9a1bpCW7GjnVQl@mahmud.db.elephantsql.com/hjvoycry';
+
+module.exports = { PG_URI };
--- server/routes/loginRouter.js
+++ server/routes/loginRouter.js
@@ -2,19 +2,42 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/',
+router.put('/company/:id',
   (_, res) => {
     return res
-      .status(200)
-      .json(res.locals.user);
+    .status(201)
+    .json(res.locals.company);
   }
 );
 
 router.post('/company/',
   (_, res) => {
     return res
-      .status(201)
-      .json(res.locals.company);
+    .status(201)
+    .json(res.locals.company);
   }
 );
 
+router.post('/applicant/:id',
+  (_, res) => {
+    return res
+      .status(200)
+      .json(res.locals.applicant);
+  }
+);
+
+router.post('/applicant/',
+  (_, res) => {
+    return res
+      .status(200)
+      .json(res.locals.applicant);
+  }
+);
+
+router.post('/',
+  (_, res) => {
+    return res
+      .status(200)
+      .json(res.locals);
+  }
+);
