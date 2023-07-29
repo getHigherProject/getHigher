e0b2cf5b0ee8e1b40f1038b504ac2e4ad4795a03
Victor Martins <martinsvictor287@gmail.com>
2023-07-30 00:02:33 +0300

    add newsletter container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,6 @@ const App = () => {
 	return (
 		<>
 			<JobsContainer />
-			<ExperienceFilter />
 		</>
 	);
 };
--- client/src/containers/JobsContainer.jsx
+++ client/src/containers/JobsContainer.jsx
@@ -1,10 +1,18 @@
+import { Box, Container, HStack } from '@chakra-ui/react';
+
 import Job from '../components/Job';
+import Newsletter from '../components/Newsletter';
 import React from 'react';
 
 const JobsContainer = () => {
 	return (
 		<>
-			<Job />
+			<Container maxW="1300px">
+				<HStack gap="78px">
+					<Job />
+					<Newsletter />
+				</HStack>
+			</Container>
 		</>
 	);
 };
