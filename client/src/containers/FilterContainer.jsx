a72037ac72aa872629b0a0468f5187504fa409c9
Victor Martins <martinsvictor287@gmail.com>
2023-07-29 22:07:01 +0300

    update filiter container

--- client/src/containers/FilterContainer.jsx
+++ client/src/containers/FilterContainer.jsx
@@ -1,28 +1,7 @@
-import {
-	Button,
-	ChevronDownIcon,
-	Menu,
-	MenuButton,
-	MenuDivider,
-	MenuGroup,
-	MenuItem,
-	MenuItemOption,
-	MenuList,
-	MenuOptionGroup,
-} from '@chakra-ui/react';
-
 import React from 'react';
 
-export default function ExperienceFilter() {
-	return (
-		<Menu>
-			<>
-				<MenuButton as={Button}>Work</MenuButton>
-				<MenuList>
-					<MenuItem>Download</MenuItem>
-					<MenuItem>Create a Copy</MenuItem>
-				</MenuList>
-			</>
-		</Menu>
-	);
-}
+const FilterContainer = () => {
+	return <div>FilterContainer</div>;
+};
+
+export default FilterContainer;
