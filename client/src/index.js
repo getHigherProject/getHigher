1ffc102f632f254d3cb88f086e973b46008cf43d
Victor Martins <martinsvictor287@gmail.com>
2023-07-29 20:51:33 +0300

    add chakra provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
 import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ChakraProvider>
+			<App />
+		</ChakraProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
