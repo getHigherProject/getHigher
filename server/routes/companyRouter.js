f303909d8633ee674b430eddf0bdd52bca0d06d4
Brian Henkel <henkel.87@gmail.com>
2023-07-31 15:33:41 -0700

    added company routes and company middlewares.

--- server/routes/companyRouter.js
+++ server/routes/companyRouter.js
@@ -5,7 +5,23 @@ const companyController = require('../controller/companyController');
 const router = express.Router();
 
 router.post('/sign-up', companyController.createCompany, (req, res) => {
-  res.status(201).json(res.locals.company);
+  return res.status(201).json(res.locals.company);
+});
+
+router.patch('/:id', companyController.updateCompany, (req, res) => {
+  return res.status(201).json(res.locals.updatedCompany);
+});
+
+router.get('/', companyController.getAllCompanies, (req, res) => {
+  return res.status(201).json(res.locals.AllCompanies);
+});
+
+router.get('/:id', companyController.getCompany, (req, res) => {
+  return res.status(201).json(res.locals.company);
+});
+
+router.delete('/:id', companyController.deleteCompany, (req, res) => {
+  return res.status(201).json(res.locals.deletedCompany);
 });
 
 module.exports = router;
