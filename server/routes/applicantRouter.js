0fd7237051288cfcfa36a1e5c31d4dd3ed64c1d0
Brian Henkel <henkel.87@gmail.com>
2023-07-31 14:10:13 -0700

    set up applicant routes and middlewares for: get all, get by id, patch, and delete

--- server/routes/applicantRouter.js
+++ server/routes/applicantRouter.js
@@ -5,7 +5,23 @@ const applicantController = require('../controller/applicantController');
 const router = express.Router();
 
 router.post('/sign-up', applicantController.createApplicant, (req, res) => {
-  res.status(201).json(res.locals.applicant);
+  return res.status(201).json(res.locals.applicant);
+});
+
+router.patch('/:id', applicantController.updateApplicant, (req, res) => {
+  return res.status(201).json(res.locals.updatedApplicant);
+});
+
+router.get('/:id', applicantController.getApplicant, (req, res) => {
+  return res.status(201).json(res.locals.applicant);
+});
+
+router.get('/', applicantController.getAllApplicants, (req, res) => {
+  return res.status(201).json(res.locals.applicants);
+});
+
+router.delete('/:id', applicantController.deleteApplicant, (req, res) => {
+  return res.status(201).json(res.locals.deletedAapplicant);
 });
 
 module.exports = router;
